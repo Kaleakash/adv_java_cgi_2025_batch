f69194b8d02845926fc85f3cef9b776953b64f7c
Akash <akash300383@gmail.com>
2025-07-29 09:44:32 +0530

    done

--- Programs/Core Java Workspace/CollectionFrameworkExamples/src/com/QueueClasses.java
+++ Programs/Core Java Workspace/CollectionFrameworkExamples/src/com/QueueClasses.java
@@ -10,13 +10,17 @@ public class QueueClasses {
 		// PriorityQueue 
 		
 		Queue q1 = new PriorityQueue();
-		q1.add(3);q1.add(1);q1.add(6);q1.add(4);q1.add(8);q1.add(7);
+		q1.add(3);q1.add(10);q1.add(6);q1.add(4);q1.add(8);q1.add(7);
+		q1.add(5);q1.add(10);
 		System.out.println(q1);
 		System.out.println("Remove element "+q1.poll());
 		System.out.println(q1);
+		
 		System.out.println("------------------");
+		
 		Queue q2 = new LinkedList();
 		q2.add(3);q2.add(1);q2.add(6);q2.add(4);q2.add(8);q2.add(7);
+		q2.add(5);q2.add(1);
 		System.out.println(q2);
 		System.out.println("Remove element "+q2.poll());
 		System.out.println(q2);
