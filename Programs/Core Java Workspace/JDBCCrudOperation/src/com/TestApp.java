75047326d21accdc95af812b564809f7450795d0
Akash <akash300383@gmail.com>
2025-07-30 10:48:45 +0530

    done

--- Programs/Core Java Workspace/JDBCCrudOperation/src/com/TestApp.java
+++ Programs/Core Java Workspace/JDBCCrudOperation/src/com/TestApp.java
@@ -2,6 +2,8 @@ package com;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class TestApp {
 
@@ -12,6 +14,17 @@ public class TestApp {
 	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cgi_db1", 
 			"root", "root@123");
 	System.out.println("connected");
+	Statement stmt = con.createStatement();
+	System.out.println("Statement object created..");
+	
+	ResultSet rs= stmt.executeQuery("select * from employee");
+		while(rs.next()) {
+		System.out.println("id is "+rs.getInt(1)+" Name is "+rs.getString(2)+" Salary is "+rs.getFloat(3));
+		}
+		
+		rs.close();
+		stmt.close();
+		con.close();
 		} catch (Exception e) {
 			System.err.println(e);
 		}
