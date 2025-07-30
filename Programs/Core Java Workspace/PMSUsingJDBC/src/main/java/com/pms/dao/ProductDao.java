388beeed7729ea464dc6e97b353e8b7a9486d615
Akash <akash300383@gmail.com>
2025-07-30 12:28:00 +0530

    done

---
+++ Programs/Core Java Workspace/PMSUsingJDBC/src/main/java/com/pms/dao/ProductDao.java
@@ -0,0 +1,36 @@
+package com.pms.dao;
+
+import java.util.List;
+
+import com.pms.bean.Product;
+
+public class ProductDao {
+
+	public int storeProduct(Product product) {
+		
+		
+		return 0;
+	}
+	
+	public int updateProduct(Product product) {
+		
+		
+		return 0;
+	}
+	
+	public int deleteProduct(int pid) {
+		
+		
+		return 0;
+	}
+	public Product findProduct(int pid) {
+		
+		
+		return null;
+	}
+	
+	public List<Product> findAllProducts() {
+		
+		return null;
+	}
+}
---
+++ Programs/Core Java Workspace/PMSUsingJDBC/src/main/java/com/pms/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.pms.service;
+
+import com.pms.dao.ProductDao;
+
+public class ProductService {
+	ProductDao pd = new ProductDao();
+	
+}
