5478b75226679658c3db6792d9a6cf72b6dc7387
Akash <akash300383@gmail.com>
2025-07-30 10:32:10 +0530

    done

--- Programs/Core Java Workspace/JDBCCrudOperation/src/com/TestApp.java
+++ Programs/Core Java Workspace/JDBCCrudOperation/src/com/TestApp.java
@@ -1,11 +1,16 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class TestApp {
 
 	public static void main(String[] args) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("Driver Loaded...");
+	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cgi_db1", 
+			"root", "root@123");
 		} catch (Exception e) {
 			System.err.println(e);
 		}
