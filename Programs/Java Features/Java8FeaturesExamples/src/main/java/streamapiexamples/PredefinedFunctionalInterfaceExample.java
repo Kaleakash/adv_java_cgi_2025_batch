c5df0d244700241f325bfb46aea8fa6824e401fc
Akash <akash300383@gmail.com>
2025-08-01 09:57:57 +0530

    done

--- Programs/Java Features/Java8FeaturesExamples/src/main/java/streamapiexamples/PredefinedFunctionalInterfaceExample.java
+++ Programs/Java Features/Java8FeaturesExamples/src/main/java/streamapiexamples/PredefinedFunctionalInterfaceExample.java
@@ -13,6 +13,7 @@ public class PredefinedFunctionalInterfaceExample {
 	public static void main(String[] args) {
 		Consumer<Integer> cc1  = new MyConumser();
 		cc1.accept(100);
+		
 		System.out.println("Providing the body for Consumer using lambda style");
 		
 	Consumer<Integer> cc2 = (n)->System.out.println("You pass the vaue as lambda style "+n);
