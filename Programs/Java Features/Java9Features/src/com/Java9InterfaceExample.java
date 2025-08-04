70dd57b285ab6feef1682082d6f1882edfd7841e
Akash <akash300383@gmail.com>
2025-08-04 10:34:14 +0530

    done

---
+++ Programs/Java Features/Java9Features/src/com/Java9InterfaceExample.java
@@ -0,0 +1,22 @@
+package com;
+interface Operation {
+	void dis1();
+	default void dis2() {
+		
+	}
+	static void dis3() {
+		dis4();
+	}
+	private static void dis4() {
+		System.out.println("private dis4 method");
+	}
+}
+
+public class Java9InterfaceExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Operation.dis3();
+	}
+
+}
