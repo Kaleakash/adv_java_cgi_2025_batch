25ea6e8851f252bdb13b19f0a0934a45d3d2bb05
Akash <akash300383@gmail.com>
2025-08-19 10:41:30 +0530

    done

--- Programs/Spring Framework workspace/DIUsingXMLBase/src/main/java/com/DemoTest.java
+++ Programs/Spring Framework workspace/DIUsingXMLBase/src/main/java/com/DemoTest.java
@@ -13,17 +13,31 @@ public class DemoTest {
 		
 		Resource res = new ClassPathResource("beans.xml");	// load the xml file 	
 		BeanFactory bean = new XmlBeanFactory(res);		// created reference of BeanFactory 
-		Employee e1	= (Employee)bean.getBean("emp1");		// pull object using id as emp1
-		e1.display();
 		
-		Employee e2	= (Employee)bean.getBean("emp1");		// pull object using id as emp1
-		e2.display();
-		System.out.println("-------------------------");
-		Employee e3	= (Employee)bean.getBean("emp2");		// pull object using id as emp1
-		e3.display();
+		// 1st example 
+//		Employee e1	= (Employee)bean.getBean("emp1");		// pull object using id as emp1
+//		e1.display();
+//		
+//		Employee e2	= (Employee)bean.getBean("emp1");		// pull object using id as emp1
+//		e2.display();
+//		System.out.println("-------------------------");
+//		Employee e3	= (Employee)bean.getBean("emp2");		// pull object using id as emp1
+//		e3.display();
+//		
+//		Employee e4	= (Employee)bean.getBean("emp2");		// pull object using id as emp1
+//		e4.display();
 		
-		Employee e4	= (Employee)bean.getBean("emp2");		// pull object using id as emp1
-		e4.display();
+		// 2nd example 
+		
+		Employee e5 = (Employee)bean.getBean("emp1");
+		System.out.println(e5);
+		
+		Employee e6 = (Employee)bean.getBean("emp2");
+		System.out.println(e6);
+		
+		Employee e7 = (Employee)bean.getBean("emp3");
+		System.out.println(e7);
+		
 	}
 
 }
--- Programs/Spring Framework workspace/DIUsingXMLBase/src/main/java/com/Employee.java
+++ Programs/Spring Framework workspace/DIUsingXMLBase/src/main/java/com/Employee.java
@@ -1,6 +1,9 @@
 package com;
 
 public class Employee {
+private int id;
+private String name;
+private float salary;
 
 	public void display() {
 		System.out.println("this is employee class method");
@@ -10,6 +13,19 @@ public class Employee {
 		super();
 		// TODO Auto-generated constructor stub
 		System.out.println("object created using empty constructor");
+	}
+
+	public Employee(int id, String name, float salary) {
+		super();
+		System.out.println("object created using parameterized constructor");
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
 	}
 	
 	
