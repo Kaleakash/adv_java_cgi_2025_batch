ce2c414409d497c215ba912fe918fadf4b6c2f2f
Akash <akash300383@gmail.com>
2025-08-21 10:48:17 +0530

    done

--- Programs/ORM Workspace/JPACrudOperation/src/main/java/com/main/DemoTest.java
+++ Programs/ORM Workspace/JPACrudOperation/src/main/java/com/main/DemoTest.java
@@ -13,15 +13,48 @@ public class DemoTest {
 	EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPACrudOperation");
 	EntityManager manager  = emf.createEntityManager();	// like Statement or PreparedStatement
 	EntityTransaction tran = manager.getTransaction();
-	Product p1 = new Product();
-	p1.setPid(100);
-	p1.setPname("TV");
-	p1.setPrice(56000);
 	
-	tran.begin();
-		manager.persist(p1);     //equal to insert query 
-	tran.commit();
-	System.out.println("Record stored...");
+	// insert query 
+//	Product p1 = new Product();
+//	p1.setPid(102);
+//	p1.setPname("Laptop");
+//	p1.setPrice(98000);
+//	
+//	tran.begin();
+//		manager.persist(p1);     //equal to insert query 
+//	tran.commit();
+//	System.out.println("Record stored...");
+	
+	// delete Query 
+//	Product productFromDb  =  manager.find(Product.class, 100);
+//	if(productFromDb==null) {
+//		System.out.println("Record not present");
+//	}else {
+//		tran.begin();
+//		manager.remove(productFromDb);
+//		tran.commit();
+//		System.out.println("Record deleted successfully");
+//	}
+	
+	// update Query 
+//	Product productFromDb  =  manager.find(Product.class, 101);
+//	if(productFromDb==null) {
+//		System.out.println("Record not present");
+//	}else {
+//		tran.begin();
+//		productFromDb.setPrice(55000);
+//		manager.merge(productFromDb);
+//		tran.commit();
+//		System.out.println("Record updated successfully");
+//	}
+	
+	// find the record using pid 
+	Product productFromDb  =  manager.find(Product.class, 101);
+	if(productFromDb==null) {
+		System.out.println("Record not present");
+	}else {
+		System.out.println(productFromDb);
+	}
 	}
 
 }
