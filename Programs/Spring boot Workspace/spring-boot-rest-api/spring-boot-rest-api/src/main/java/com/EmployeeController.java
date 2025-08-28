7ed87c9dc784cacf35306ebf65683dad5e10253a
Akash <akash300383@gmail.com>
2025-08-28 11:07:52 +0530

    done

--- Programs/Spring boot Workspace/spring-boot-rest-api/spring-boot-rest-api/src/main/java/com/EmployeeController.java
+++ Programs/Spring boot Workspace/spring-boot-rest-api/spring-boot-rest-api/src/main/java/com/EmployeeController.java
@@ -1,11 +1,14 @@
 package com;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,9 +37,39 @@ public class EmployeeController {
 	
 	// http://localhost:8080/employees
 	
-	@RequestMapping(value = "employees",method = RequestMethod.GET,
+	@RequestMapping(value = "employees",method = RequestMethod.GET,	
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Employee> findAllEmployees() {
 		return listOfEmp;
 	}
+	
+	// http://localhost:8080/findEmployeeByQueryParam?id=100
+	// http://localhost:8080/findEmployeeByQueryParam?id=1000
+	
+	@RequestMapping(value = "findEmployeeByQueryParam")
+	public String findEmployeeUsingQueryParam(@RequestParam("id") int id) {
+	Iterator<Employee> li = listOfEmp.iterator();
+	while(li.hasNext()) {
+		Employee emp = li.next();
+		if(emp.getId()==id) {
+			return emp.toString();
+		}
+	}
+		return "Employee not present";
+	}
+		// http://localhost:8080/findEmployeeByPathParam/100
+		// http://localhost:8080/findEmployeeByPathParam/1000
+	
+		@RequestMapping(value = "findEmployeeByPathParam/{id}")
+		public String findEmployeeUsingPathParam(@PathVariable("id") int id) {
+		Iterator<Employee> li = listOfEmp.iterator();
+		while(li.hasNext()) {
+			Employee emp = li.next();
+			if(emp.getId()==id) {
+				return emp.toString();
+			}
+		}
+			return "Employee not present";
+		}
+	
 }
