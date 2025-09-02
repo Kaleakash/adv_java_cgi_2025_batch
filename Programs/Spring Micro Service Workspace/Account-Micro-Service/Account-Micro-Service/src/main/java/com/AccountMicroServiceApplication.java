ebf45592e0472b21ba2428f8a5d4e01b2400ad62
Akash <akash300383@gmail.com>
2025-09-02 12:30:47 +0530

    done

--- Programs/Spring Micro Service Workspace/Account-Micro-Service/Account-Micro-Service/src/main/java/com/AccountMicroServiceApplication.java
+++ Programs/Spring Micro Service Workspace/Account-Micro-Service/Account-Micro-Service/src/main/java/com/AccountMicroServiceApplication.java
@@ -2,12 +2,19 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")  // Enable @Restcontroller, @Service, @Repository 
+@EntityScan(basePackages = "com.entity")  // Enable @Entity annotation 
+@EnableJpaRepositories(basePackages = "com.repository") // enable spring data features 
+@EnableDiscoveryClient
 public class AccountMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountMicroServiceApplication.class, args);
+		System.err.println("Account micro service up on port number 8383");
 	}
 
 }
---
+++ Programs/Spring Micro Service Workspace/Account-Micro-Service/Account-Micro-Service/src/main/java/com/controller/AccountController.java
@@ -0,0 +1,43 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.entity.Account;
+import com.service.AccountService;
+
+@RestController
+@RequestMapping("account")   // http://localhost:8383/account/*
+public class AccountController {
+
+	@Autowired
+	AccountService accountService;
+	
+	
+//	@RequestMapping(value = "create",
+//			method = RequestMethod.POST,
+//			consumes = MediaType.APPLICATION_JSON_VALUE)
+
+	@PostMapping(value = "create",consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String createAccount(@RequestBody Account Account) {
+		return accountService.createAccount(Account);
+	}
+	
+	@GetMapping(value = "findBalance/{accno}")
+	public String findBalance(@PathVariable int accno) {
+		return accountService.findAccountBalance(accno);
+	}
+
+	
+	@GetMapping(value = "findAccount/{emailId}")
+	public int findBalance(@PathVariable String emailId) {
+		return accountService.findAccountNumbre(emailId);
+	}
+}
