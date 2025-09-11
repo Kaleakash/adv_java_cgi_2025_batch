6bcd0a4cf74b63863e4f34043ec524d7f0489276
Akash <akash300383@gmail.com>
2025-09-11 11:52:31 +0530

    done

--- Programs/Spring boot With Apache Kafka/spring-boot-apache-kafka-producer/spring-boot-apache-kafka-producer/src/main/java/com/SpringBootApacheKafkaProducerApplication.java
+++ Programs/Spring boot With Apache Kafka/spring-boot-apache-kafka-producer/spring-boot-apache-kafka-producer/src/main/java/com/SpringBootApacheKafkaProducerApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com")
-public class SpringBootApacheKafkaConsumerApplication {
+public class SpringBootApacheKafkaProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApacheKafkaConsumerApplication.class, args);
-		System.out.println("kafka Producer application up! on port number 8181");
+		SpringApplication.run(SpringBootApacheKafkaProducerApplication.class, args);
+		System.out.println("kafka Producer application up! on port number 8282");
 	}
 
 }
